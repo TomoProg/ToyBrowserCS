8b21a00ebb892184f29038bc13b3b0796e3c870b
TomoProg <helloworld0306.xxx@gmail.com>
2019-09-04 22:05:12 +0900

    robinsonをC#で実装

---
+++ Main/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyBrowserCS;
+using System.Drawing;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = System.IO.File.ReadAllText("sample.html");
+            var css = System.IO.File.ReadAllText("sample.css");
+
+            var viewport = new Layout.Dimensions();
+            viewport.Content.Width = 800;
+            viewport.Content.Height = 600;
+
+            var rootNode = Html.Parse(html);
+            var stylesheet = Css.Parse(css);
+            var styleRoot = Style.StyleTree(rootNode, stylesheet);
+            var layoutRoot = Layout.LayoutTree(styleRoot, viewport);
+
+            var d = new Layout.Dimensions();
+            d.Content.Width = 800;
+            d.Content.Height = 600;
+            var canvas = Painting.Paint(layoutRoot, d.Content);
+
+            var bitmap = new Bitmap(800, 600);
+            for(int x = 0; x < canvas.Width; x++)
+            {
+                for(int y = 0; y < canvas.Height; y++)
+                {
+                    var px = canvas.Pixels[y * canvas.Width + x];
+                    bitmap.SetPixel(x, y, Color.FromArgb(px.A, px.R, px.G, px.B));
+                }
+            }
+            bitmap.Save("result.png");
+        }
+    }
+}
